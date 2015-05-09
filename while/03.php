4ba344bd86e7da577f49bece2d7fa994d923cf8c
Ion <megaivmay15@gmail.com>
2015-05-09 15:45:19 +0400

    comment

--- while/03.php
+++ while/03.php
@@ -3,4 +3,3 @@
  * Two positive integers N and K are given. Using addition and subtraction only,
  * find a quotient of the integer division N on K and also a remainder after this division.
 */
-kjh
--- while/04.php
+++ while/04.php
@@ -6,5 +6,18 @@
 $n = 9;
 const a = 3;
 while ( $n >= a){
-    if ( )
+    if ( pow(a , 1)== $n){
+        echo 'It is true <br/>';
+    } else {
+        echo 'It is false <br/>';
+    }  if (pow(a,2) == $n){
+        echo 'It is true <br/>';
+    } else {
+        echo ' It is false <br/>';
+    } if (pow(a,3)== $n){
+        echo 'It is true <br/>';
+    } else {
+        echo 'It is false <br/>';
+    }
+    break;
 }
